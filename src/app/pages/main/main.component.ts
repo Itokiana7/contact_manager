7850e748c9fe29e6ba3591648a2c1072934e65c6
Itokiana7 <rabeitokiana@gmail.com>
2025-07-11 12:01:26 +0100

    second

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { ContentComponent } from '../../components/content/content.component';
+import { AboutComponent } from '../../components/about/about.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    SidebarComponent,
+    ContentComponent,
+    AboutComponent
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.css'
 })
 export class MainComponent {
 
