d7734bff904ac1fcd6b6156ee15d652d3533e899
Itokiana7 <rabeitokiana@gmail.com>
2025-07-15 09:36:04 +0100

    ui : sidebar loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{html,ts}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+const preline = require('preline/plugin')
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/preline/**/*.js"  // ajoute cette ligne
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [preline],  // ajoute le plugin
 }
 
